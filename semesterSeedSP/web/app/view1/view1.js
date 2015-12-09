d603b9274b3ef9768380fc05ef33021f8cbd058d
ichti <snotslot@gmail.com>
2015-12-09 11:15:25 +0100

    Fixed date in search

--- semesterSeedSP/web/app/view1/view1.js
+++ semesterSeedSP/web/app/view1/view1.js
@@ -10,9 +10,7 @@ angular.module('myApp.view1', ['ngRoute'])
                 });
             }])
 
-        .controller('View1Ctrl', ["InfoFactory", "InfoService", "$resource", "$scope", function (InfoFactory, InfoService,  $resource, $scope) {
-                this.msgFromFactory = InfoFactory.getInfo();
-                this.msgFromService = InfoService.getInfo();
+        .controller('View1Ctrl', ["$resource", "$scope", function ($resource, $scope) {
                 // Define flights resource
                 var From = $resource("/semesterSeedSP/api/flights/:from/:date/:tickets", {from: "@from", date: "@date", tickets: "@tickets"}, {});
                 var FromTo = $resource("/semesterSeedSP/api/flights/:from/:to/:date/:tickets", {from: "@from", to: "@to", date: "@date", tickets: "@tickets"}, {});
@@ -20,21 +18,18 @@ angular.module('myApp.view1', ['ngRoute'])
                 $scope.search = {from: "", to: "NULL", date: "", tickets: 1};
 
                 $scope.click = function () {
-                    var myDate = new Date($scope.search.date);
-                    var year = myDate.getFullYear();
-                    var month = myDate.getMonth();
-                    var day = myDate.getDate();
-                    var date = new Date(year, month, day, 1);
+                    var dateNums = $scope.search.date.split(".");
+                    var isodate = new Date(dateNums[2], dateNums[1]-1, dateNums[0], 1);
                     if ($scope.search.to === "NULL") {
                         var restArguments = {from: $scope.search.from, 
-                                             date: date.toISOString(), 
+                                             date: isodate.toISOString(), 
                                              tickets: $scope.search.tickets};
                         $scope.flights = From.query(restArguments);
                     } 
                     else {
                         var restArguments = {from: $scope.search.from, 
                                              to: $scope.search.to,
-                                             date: date.toISOString(), 
+                                             date: isodate.toISOString(), 
                                              tickets: $scope.search.tickets};
                         $scope.flights = FromTo.query(restArguments);
                     }
