f82ee02744cdfa91e3f9f4e0a8ec4c2ac3ff2552
ichti <snotslot@gmail.com>
2015-12-09 09:48:29 +0100

    search almost done

--- semesterSeedSP/web/app/app.js
+++ semesterSeedSP/web/app/app.js
@@ -1,4 +1,4 @@
-	
+var test;
 
     'use strict';
      
@@ -24,41 +24,3 @@
             config(function ($httpProvider) {
                 $httpProvider.interceptors.push('authInterceptor');
             });
-     
-    myApp.controller("RestCtrl", ["$scope", "$resource", function ($scope, $resource) {
-     
-            // Define flights resource
-            var From = $resource("/semesterSeedSP/api/flights/:from/:date/:tickets", {from: "@from", date: "@date", tickets: "@tickets"}, {});
-            var FromTo = $resource("/semesterSeedSP/api/flights/:from/:to/:date/:tickets", {from: "@from", to: "@to", date: "@date", tickets: "@tickets"}, {});
-           
-            //$scope.fromLOL = {from: "", date: "", tickets: ""};
-            $scope.fromToLOL = {from: "", to: "NULL", date: "", tickets: ""};
-            $scope.onlyFrom = {from: $scope.fromToLOL.from.valueOf(), date: $scope.fromToLOL.date.valueOf(), tickets: $scope.fromToLOL.tickets.valueOf()};
-           
-            $scope.click = function(){
-                if($scope.fromToLOL.to === "NULL"){
-                    $scope.fromToLOL.to.
-                    searchWODestination();                
-                }else{
-                    searchWithDestination();
-                }
-               
-            };
-           
-            // -- MÅFÅKAH MAN : ((
-           
-           
-            var searchWODestination = function () {
-               
-                $scope.flights = From.query($scope.onlyFrom);
-                console.log($scope.onlyFrom);
-               
-            };
-           
-            var searchWithDestination = function(){
-                $scope.flight = FromTo.query($scope.fromToLOL);
-                console.log($scope.fromToLOL);
-            }
-     
-        }]);
-
--- semesterSeedSP/web/app/view1/view1.js
+++ semesterSeedSP/web/app/view1/view1.js
@@ -2,15 +2,41 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'app/view1/view1.html',
+                    controller: 'View1Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+        .controller('View1Ctrl', ["InfoFactory", "InfoService", "$resource", "$scope", function (InfoFactory, InfoService,  $resource, $scope) {
+                this.msgFromFactory = InfoFactory.getInfo();
+                this.msgFromService = InfoService.getInfo();
+                // Define flights resource
+                var From = $resource("/semesterSeedSP/api/flights/:from/:date/:tickets", {from: "@from", date: "@date", tickets: "@tickets"}, {});
+                var FromTo = $resource("/semesterSeedSP/api/flights/:from/:to/:date/:tickets", {from: "@from", to: "@to", date: "@date", tickets: "@tickets"}, {});
+                
+                $scope.search = {from: "", to: "NULL", date: "", tickets: 1};
+
+                $scope.click = function () {
+                    var myDate = new Date($scope.search.date);
+                    var year = myDate.getFullYear();
+                    var month = myDate.getMonth();
+                    var day = myDate.getDate();
+                    var date = new Date(year, month, day, 1);
+                    if ($scope.search.to === "NULL") {
+                        var restArguments = {from: $scope.search.from, 
+                                             date: date.toISOString(), 
+                                             tickets: $scope.search.tickets};
+                        $scope.flights = From.query(restArguments);
+                    } 
+                    else {
+                        var restArguments = {from: $scope.search.from, 
+                                             to: $scope.search.to,
+                                             date: date.toISOString(), 
+                                             tickets: $scope.search.tickets};
+                        $scope.flights = FromTo.query(restArguments);
+                    }
+                };
+            }]);
